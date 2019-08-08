09f6b569304a711e5be30681dd30c775c129b5c0
chirsz <chirsz@foxmail.com>
2019-08-08 14:56:04 +0800

    split svg attributes to css

--- suta/suta_generate.py
+++ suta/suta_generate.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 
 from math import sqrt
 import sys
@@ -7,19 +7,21 @@ import jinja2
 suta_svg_template = jinja2.Template(
 '''
 <?xml version="1.0" standalone="no"?>
-<!-- generated with {{ __file__ }} {{ dx1 }} {{ dx2 }} -->
+<!-- generated with `{{ __file__ }} {{ ' '.join(sys.argv[1:]) }}` -->
 
-<svg width="{{ w }}" height="{{ w }}" xmlns="http://www.w3.org/2000/svg">
-  <path d="M {{ (w-l)/2 }} {{ h1+(w-l)/2 }}
+<svg id="suta-symbol" width="{{ w }}" height="{{ w }}" xmlns="http://www.w3.org/2000/svg">
+  <style>
+{{ style_sheets }}
+  </style>
+  <path class="suta-path"
+        d="M {{ (w-l)/2 }} {{ h1+(w-l)/2 }}
            h {{ l }}
            c {{ -dx1 }} {{ dy1 }} {{ dx2-l/4 }} {{ dh }} {{ -l/4 }} {{ dh }}
            s {{ dx1-l/4 }} {{ dy1-dh }} {{ -l/4 }} {{ -dh }}
            l {{ -d }} {{ -h1 }}
            v {{ l }}
            l {{ d+l/4 }} {{ -l }}
-           v {{ l }} z"
-        stroke-width="3" stroke="#000" fill="none"
-        stroke-linejoin="round"/>
+           v {{ l }} z" />
 </svg> 
 '''.lstrip())
 
@@ -31,5 +33,12 @@ h1 = l/(2*φ-1)
 d  = l*(φ-1)/2
 dy1 = dx1*h1/d
 dh = (l-h1)*(2/(2*φ+1)*(2-φ)+2*(2-φ)/3*(φ-1))
+
+# attach style sheets
+style_sheets = ''
+for f in sys.argv[3:]:
+  with open(f) as cssf:
+    for line in cssf.readlines():
+      style_sheets += '    ' + line
 
 print(suta_svg_template.render(locals()))
